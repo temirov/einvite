50df32e939cf3d0f36c92fbf00d526d5ef6735ab
Vadym Tyemirov <temirov@gmail.com>
2013-03-07 16:54:28 -0500

    DB migration pertaining to Auth table have been removed

--- db/migrate/20130228022854_add_authorized_to_auth.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorizedToAuth < ActiveRecord::Migration
-  def change
-    add_column :auths, :authorized, :boolean
-  end
-end
--- db/migrate/20130302180105_drop_auths_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAuthsTable < ActiveRecord::Migration
-  def up
-    drop_table :auths
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
