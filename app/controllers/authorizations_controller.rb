021a97f9f80a819c57aebf06faf547792a593e01
Vadym Tyemirov <temirov@gmail.com>
2013-03-08 01:38:12 -0500

    Create method of AuthorizationController improved to handle redirection to the initial path after successful authorization

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -3,6 +3,7 @@ class AuthorizationsController < ApplicationController
   # GET /authorizations/new
   # GET /authorizations/new.json
   def new
+    debugger
     @authorization = Authorization.new
 
     respond_to do |format|
@@ -12,26 +13,30 @@ class AuthorizationsController < ApplicationController
   end
 
   def create
-#    debugger
-    email = params[:authorization][:user][:email].downcase
-    user = User.find_by_email(email)
-    if user
-      if authorization.find_by_session_token(cookies[:session_token]).try(:authenticate, params[:password])
-        # Sign the user in and redirect to the user's show page.
-        sign_in
-        redirect_to root_url
+    debugger
+    @user = User.find_by_email(params[:authorization][:user_attributes][:email].downcase)
+    if @user
+      if @user.authorization.try(:authenticate, params[:authorization][:user_attributes][:password])
+        # Sign the user in and redirect to the requested page.
+        # redirect_to root_url
+        successful_authorization
       else
-        flash.now[:error] = 'Invalid email/password combination'
-        render 'new'
+        debugger
+        # flash.now[:error] = 'Invalid email/password combination'
+        # render 'new'
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     else
       @authorization = Authorization.new(params[:authorization])
 
       respond_to do |format|
         if @authorization.save
-          sign_in @authorization.user
-          format.html { redirect_to @authorization, notice: 'Authorization was successfully created.' }
-          format.json { render json: @authorization, status: :created, location: @authorization }
+          successful_authorization
+          # redirect_to root_url
+          # sign_in @authorization.user
+          # format.html { redirect_to session[:return_to], notice: 'Authorization was successfully created.' }
+          # format.json { render json: @authorization, status: :created, location: @authorization }
         else
           format.html { render action: "new" }
           format.json { render json: @authorization.errors, status: :unprocessable_entity }
@@ -44,4 +49,10 @@ class AuthorizationsController < ApplicationController
     sign_out
     redirect_to root_url
   end
+
+  private
+    def successful_authorization
+      sign_in @user
+      redirect_to session.delete(:return_to), notice: 'Authorization was successful'
+    end
 end
