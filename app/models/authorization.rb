5457dfe4ecfcf4ab549452160e4290edfc643783
Vadym Tyemirov <temirov@gmail.com>
2013-03-12 23:27:45 -0400

    Inverse relationship to user model added

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,14 +1,16 @@
 class Authorization < ActiveRecord::Base
   has_secure_password
   
-  belongs_to :user
+  belongs_to :user, :inverse_of => :authorization
   accepts_nested_attributes_for :user
   attr_accessor :email
 
   attr_accessible :password, :password_confirmation, :session_token, :new_password, :email, :user_attributes
 
   after_initialize :build_user, :unless => :user 
-  before_save :create_session_token
+  before_save :create_session_token 
+  # after_create :update_user_id
+  before_create :create_session_token, :update_user_id
 
   def new_password
     (0...4).map{ SecureRandom.random_number(10) }.join
@@ -18,6 +20,10 @@ class Authorization < ActiveRecord::Base
 
     def create_session_token
       self.session_token = SecureRandom.urlsafe_base64
+    end
+
+    def update_user_id
+      user.authorization ||= self
     end
 
     def create_password
