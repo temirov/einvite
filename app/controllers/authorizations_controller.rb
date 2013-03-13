263c4c6c14dcbfb8f1d528f6039efdcd5dedda62
Vadym Tyemirov <temirov@gmail.com>
2013-03-13 01:24:08 -0400

    User's redirection is fixed (respond_to block was unnecessary)

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -15,13 +15,10 @@ class AuthorizationsController < ApplicationController
     @user = User.find_by_email(params[:authorization][:user_attributes][:email].downcase)
     if @user
       if @user.authorization.try(:authenticate, params[:authorization][:password])
-        # Sign the user in and redirect to the requested page.
-        # redirect_to root_url
         successful_authorization @user
       else
         @authorization = @user.authorization
-        # flash.now[:error] = 'Invalid email/password combination'
-        # render 'new'
+
         respond_to do |format|
           format.html { render action: "new" }
           format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -29,15 +26,10 @@ class AuthorizationsController < ApplicationController
       end
     else
       @authorization = Authorization.new(params[:authorization])
-
-      respond_to do |format|
-        if @authorization.save
-          successful_authorization @authorization.user
-          # redirect_to root_url
-          # sign_in @authorization.user
-          # format.html { redirect_to session[:return_to], notice: 'Authorization was successfully created.' }
-          # format.json { render json: @authorization, status: :created, location: @authorization }
-        else
+      if @authorization.save
+        successful_authorization @authorization.user
+      else
+        respond_to do |format|
           format.html { render action: "new" }
           format.json { render json: @authorization.errors, status: :unprocessable_entity }
         end
@@ -56,7 +48,6 @@ class AuthorizationsController < ApplicationController
       # redirect_to session[:return_to], notice: 'Authorization was successful'
       # session.delete(:return_to)
       # redirect_to :back, notice: 'Authorization was successful'
-      debugger
       redirect_to edit_user_path(user.id)
     end
 end
