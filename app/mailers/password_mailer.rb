942f708fbbd550daa93ccbebcded38e12e0ca511
Vadym Tyemirov <temirov@gmail.com>
2013-03-18 20:09:11 -0400

    debugger removed

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,7 +2,6 @@ class PasswordMailer < ActionMailer::Base
   default from: "eInvite@temirov.net"
 
   def password_email(authorization)
-    debugger
     @authorization = authorization
     @url  = 'http://localhost:3000/'
     mail(to: authorization.user.email, subject: 'eInvite Registration: your password')
