9f92e37d47ce98d581079657ea0d6154ba696def
Vadym Tyemirov <temirov@gmail.com>
2013-03-24 15:24:17 -0400

    AUtomatic detection of current URL added; site changed to Chirpathon.com

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -3,7 +3,8 @@ class PasswordMailer < ActionMailer::Base
 
   def password_email(authorization)
     @authorization = authorization
-    @url  = 'http://localhost:3000/'
-    mail(to: @authorization.user.email, subject: 'Chirpathon Registration: your password')
+    @url = "#{request.protocol}#{request.fullpath}"
+    # @url  = 'http://localhost:3000/'
+    mail(to: @authorization.user.email, subject: 'Chirpathon registration: your password')
   end
 end
