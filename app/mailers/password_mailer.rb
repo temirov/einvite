6f40177d6c91eaf331ef389fe346396b0d9566fd
Vadym Tyemirov <temirov@gmail.com>
2013-03-24 16:57:24 -0400

    default_url_options added; variable's expansion moved to PasswordMailer class

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -3,8 +3,9 @@ class PasswordMailer < ActionMailer::Base
 
   def password_email(authorization)
     @authorization = authorization
-    @url = "#{request.protocol}#{request.fullpath}"
-    # @url  = 'http://localhost:3000/'
-    mail(to: @authorization.user.email, subject: 'Chirpathon registration: your password')
+    @email = @authorization.user.email
+    @password = @authorization.new_password
+    @login_url  = edit_authorization_url(@authorization)
+    mail(to: @email, subject: 'Chirpathon registration: your password')
   end
 end
