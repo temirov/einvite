7f859787df65dc080642cf623c96c04f0c5ff0f1
Vadym Tyemirov <temirov@gmail.com>
2013-03-26 16:24:19 -0400

    Password timing check added, code refactored

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -18,50 +18,39 @@ class AuthorizationsController < ApplicationController
   def update
     @authorization = Authorization.find(params[:id])
 
-    respond_to do |format|
-      reset_session
-      if @authorization.try(:authenticate, params[:authorization][:password])
-        sign_in @authorization
-        format.html { redirect_to edit_user_path(@authorization.user), notice: 'Logged in successfully' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @authorization.errors, status: :unprocessable_entity }
+    if @authorization.recent?
+      respond_to do |format|
+        reset_session
+        if @authorization.try(:authenticate, params[:authorization][:password])
+          sign_in @authorization
+          format.html { redirect_to edit_user_path(@authorization.user), notice: 'Logged in successfully' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @authorization.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      unsuccessful_authorization
     end
   end
 
   def create
     @authorization = Authorization.includes(:user).where("users.email" => params[:authorization][:user_attributes][:email].downcase).first
-    # TODO: redundant code, refactor!
 
-    respond_to do |format|
-      if @authorization.present?
-        authorization_redirect(@authorization.update_attributes(params[:authorization]))
-#        if @authorization.update_attributes(params[:authorization])
-#          format.html { redirect_to edit_authorization_path(@authorization), notice: 'Password was sent successfully.' }
-#          format.json { render json: @authorization, status: :created, location: @authorization }
-#        else
-#          format.html { render action: "new" }
-#          format.json { render json: @authorization.errors, status: :unprocessable_entity }
-#        end
-      else
-        @authorization = Authorization.new(params[:authorization])
-        authorization_redirect(@authorization.save)
-#        if @authorization.save
-#          format.html { redirect_to edit_authorization_path(@authorization), notice: 'Password was sent successfully.' }
-#          format.json { render json: @authorization, status: :created, location: @authorization }
-#        else
-#          format.html { render action: "new" }
-#          format.json { render json: @authorization.errors, status: :unprocessable_entity }
-#        end
-      end
+    if @authorization.present?
+      authorization_redirect(@authorization.update_attributes(params[:authorization]))
+    else
+      @authorization = Authorization.new(params[:authorization])
+      authorization_redirect(@authorization.save)
     end
   end
   
   def destroy
     sign_out
-    redirect_to root_url
+    respond_to do |format|
+      format.html { redirect_to root_url }
+    end
   end
 
   private
